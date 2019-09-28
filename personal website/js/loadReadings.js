c965365621755383038b593116570ab96cbe7c45
Sylvain Tran <45033804+peelingSun@users.noreply.github.com>
2019-09-28 18:34:44 -0400

    P1: Added functionality for loading the readings into the single page from an xml file.
    P1: Added functionality for loading the readings into the single page from an xml file.

--- personal website/js/loadReadings.js
+++ personal website/js/loadReadings.js
@@ -1,36 +1,64 @@
+// the readings' data from the XML file
+let dataContainer;
+console.log("Creating... a connection...");
+
 function createConnection(){
+	console.log("Attempting to fetch the XML file...");
+
 	let xhr = new XMLHttpRequest();
 	let xml = xhr.responseXML;
+	//Opens the xml file asynchronously
+
+	$.ajax(
+		{
+			url: '../readings.xml',
+			type: 'GET',
+			dataType: 'xml',
+			timeout: 1000,
+			error: function(){
+				alert('Error loading the xml file.'); 
+			},
+			success: function(data){
+				console.log("Successfully fetched the XML file.");				
+				dataContainer = data;
+			}
+		}
+	);
 }
 
-function LoadXML(xml, readingsDisplay)
-{
-    //Opens the xml file
+/**
+ 
+	Fill the readingsDisplay div after the user clicks on the readings in the menu.
+	
+*/
+function displayReadings(){
+	console.log("Loading the XML into the website...");				
 
-	try{
-		xhr.onreadystatechange = function()
-		{
-			if (this.readyState == 4 && this.status == 200)
-			{
-                xhr.open("GET", "./readings/readings.xml", true);
-                xhr.send();		
+    let notebook = dataContainer;
+	let readings = notebook.getElementsByTagName("readings");
+	let readingDisplay = document.getElementById('readingsDisplay');
+	console.log("Number of readings found: " + readings[0].childNodes.length);
+
+	// Create a new div element for each reading.
+	for(let i = 0; i < readings.length; i++){
+		if(readings[i].hasChildNodes()){
+			// loop through each each class's one or two readings
+			let children = readings[i].childNodes;
+			console.log("reading #" + i);
+
+			for(let j = 0; j < children.length; j++){
+				if(children[j].nodeName === "firstReading" || children[j].nodeName === "secondReading")
+				{
+					console.log("...reading # " + j + "...of reading # " + i);
+					readingDisplay.append(children[j]);
+				}
 			}
 		}
 	}
-	catch(e)
-	{
-		alert("Error with html http request...");
-    }
-    
-    let notebook = xml.responseXML;
-    let readings = notebook.getElementsByTagName("reading");
-    
-    /**
-     
-        Fill the readingsDisplay div.
-     
-    */
-	for(let i = 0; i < readings.length; i++)
+
+	// Parse, beginning from the first reading, each of them into their divs
+
+/* 	for(let i = 0; i < readings.length; i++)
 	{
 		let imgProd = document.createElement("img");
 		let imgSrc = readings[i].childNodes[5].childNodes[0].textContent; //<img><src> x </src></img>
@@ -48,5 +76,5 @@ function LoadXML(xml, readingsDisplay)
 		arrayProd.rows[2].cells[i].innerHTML = x[i].childNodes[1].textContent;
 		arrayProd.rows[3].cells[i].innerHTML = x[i].childNodes[2].textContent;
 		arrayProd.rows[4].cells[i].innerHTML = x[i].childNodes[3].textContent;
-	}
+	} */
 }
