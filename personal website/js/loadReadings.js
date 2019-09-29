6b4dd0a57338a7e8d6d1aec091a7c65c4d68b33b
Sylvain Tran <45033804+peelingSun@users.noreply.github.com>
2019-09-29 00:29:12 -0400

    P1: Added a function that adds a tree display (table of readings) and accordeon for the readings.
    P1: Added a function that adds a tree display (table of readings) and accordeon for the readings.

--- personal website/js/loadReadings.js
+++ personal website/js/loadReadings.js
@@ -36,7 +36,10 @@ function displayReadings(){
 
     let notebook = dataContainer;
 	let readings = notebook.getElementsByTagName("readings");
-	let readingDisplay = document.getElementById('readingsDisplay');
+	let mainDisplay = document.getElementsByClassName('mainDisplay')[0];
+	let readingsDisplay = document.createElement('div');
+	readingsDisplay.classList.add('readingsDisplay');
+	mainDisplay.append(readingsDisplay);
 	console.log("Number of readings found: " + readings[0].childNodes.length);
 
 	// Create a new div element for each reading.
@@ -50,12 +53,104 @@ function displayReadings(){
 				if(children[j].nodeName === "firstReading" || children[j].nodeName === "secondReading")
 				{
 					console.log("...reading # " + j + "...of reading # " + i);
-					readingDisplay.append(children[j]);
+					readingsDisplay.append(children[j]);
 				}
 			}
 		}
 	}
+}
 
+/**
+ 
+	Displays accordion for the table of readings.
+	
+*/
+function loadTableOfReadings(){
+	//Tree view
+	let treeParent = document.createElement('div');
+	let mainDisplay = document.getElementsByClassName('mainDisplay')[0];
+	mainDisplay.append(treeParent);
+
+	treeParent.classList.add("treeDisplay");
+	let fieldsetTree = document.createElement("FIELDSET");
+	fieldsetTree.setAttribute("id", "fieldsetTreeView");
+	let tree = "<ul id=\"readingsTreeView\">" +
+				    "<li><span class=\"caret\">First Class</span>" +
+			   "<ul class=\"nested\">" + //beginning of nested ul
+					"<li>Roy Rosenzweig, Writing the History of the Internet</li>" +
+					"<li>Daniel Joseph, The Time Canada Wanted Its Own Internet Because" +
+					"It Thought the US Would Mess It Up" + 
+					"</li>" + 
+					"<li><span class=\"caret\">Fred Turner, From Counterculture to Cyberculture</span>" +
+					"<li>Rachel Greene, Web Work: A History of Internet Art</li>" +
+					"<li>Carolina Miranda, The New World of Net Art</li>" + 
+				"</ul>";//end of nested ul
+	fieldsetTree.innerHTML = tree;
+	treeParent.appendChild(fieldsetTree);
+}
+
+function loadAccordion(){
+	let readingDisplay = document.getElementById('readingsDisplay');
+	//Insertion of xml-parsed lore database
+	var mainDisplay = document.getElementById("MainDisplay");
+	var readingTitles = document.getElementsByTagName("readings")[0];
+	var xLen = readingTitles.childNodes.length;
+	var y = readingTitles.firstChild;
+
+	var titles = "";
+	for(let i = 0; i < xlen; i++)
+	{
+		if(y.nodeType == 1)
+		{
+			titles += y.nodeName.toUpperCase();
+			if(y.nodeName != "readings") titles += ",";
+		}
+		y = y.nextSibling;
+	}
+
+	//parse the titles string
+	let tokenTitles = titles.split(",");
+	var titlesArray = [];
+
+	for(let i = 0; i < tokenTitles.length; i++)
+	{
+		titlesArray[i] = document.createElement("H5");
+		//set an id attribute for event handling
+		titlesArray[i].setAttribute("id", tokenTitles[i]);
+		//titlesArray[i].setAttribute("onclick", "UnfoldChildren(this, readingTitles, xlen);");
+		let textBlock = document.createTextNode(tokenTitles[i]);		
+		titlesArray[i].appendChild(textBlock);
+	}	
+
+	for(title in titlesArray )
+	{
+		var accordionBt = document.createElement("BUTTON");
+		accordionBt.setAttribute("class", "accordion");
+		var panel = document.createElement("PANEL");
+		panel.setAttribute("class", "panel");
+		accordionBt.appendChild(titlesArray[title]);	
+		readingDisplay.appendChild(accordionBt);
+		readingDisplay.appendChild(panel);
+	}	
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for(i = 0; i < acc.length; i++)
+	{
+		acc[i].addEventListener("click", function(){
+			this.classList.toggle("active");
+			var panelAcc = this.nextElementSibling;;
+			if(panelAcc.style.maxHeight)
+			{
+				panelAcc.style.maxHeight = null;
+			}
+			else
+			{
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		});
+	}	
+}
 	// Parse, beginning from the first reading, each of them into their divs
 
 /* 	for(let i = 0; i < readings.length; i++)
@@ -77,4 +172,3 @@ function displayReadings(){
 		arrayProd.rows[3].cells[i].innerHTML = x[i].childNodes[2].textContent;
 		arrayProd.rows[4].cells[i].innerHTML = x[i].childNodes[3].textContent;
 	} */
-}
