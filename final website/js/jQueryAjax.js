ab7769afe1f409443c67fb477f1be4cd05e2768a
SylvainTran <sylvain.serey.tran@protonmail.com>
2019-11-12 22:52:20 -0500

    final website: Added twine div (tw-story) inside the right side div, and some data handling on the tabs (as a test, for all except the phone tab) by using onclick() functions and calls to the jQueryAjax.js script.
    Situation: Needed to figure out a way to append the twine story on the website and start data handling.
    Changes: Added twine div (tw-story) inside the right side div, and some data handling on the tabs (as a test, for all except the phone tab) by using onclick() functions and calls to the jQueryAjax.js script.

--- final website/js/jQueryAjax.js
+++ final website/js/jQueryAjax.js
@@ -1,4 +1,8 @@
 let sceneData, sceneConfig;
+let consoleLogDefaultText = "Microsoft Windows [Version 10.0.17763.805] <br>" +
+        "(c) 2018 Microsoft Corporation. All rights reserved. <br> <br>" +
+        "C:\\Users\\jdesplan>";
+let inventoryStatus;
 /*
 	Asynchronously, using Jquery Ajax, fetches the sceneData and sceneConfig.json.
 
@@ -41,4 +45,32 @@ function getSceneConfig(){
 			}
 		}
 	);
+}
+/*
+	Gets the inventory from sceneData.json (for now, but will be using local storage later).
+	Outputs it in the console log area below the Twine canvas. The variable is at the top of the scope for access in other tabs.
+
+*/
+function getInventory(){
+	console.log("Attempting to fetch the inventory from the data stored earlier...");
+	inventoryStatus = consoleLogDefaultText + " " + sceneData.inventory.items + sceneData.inventory.equipment + sceneData.inventory.spells;
+	document.querySelector('.footer__console-status').innerHTML = inventoryStatus;
+}
+/*
+	Gets the character stats from sceneData.json (for now, but will be using local storage later).
+	Outputs it in the console log area below the Twine canvas.
+
+*/
+function getCharacterStats(){
+	console.log("Attempting to fetch the character stats from the data stored earlier...");
+	document.querySelector('.footer__console-status').innerHTML = consoleLogDefaultText + " " + "Acuity: " + sceneData.characterStats.acuity + " Courage: " + sceneData.characterStats.courage + " Resistance: " + sceneData.characterStats.resistance + " Wisdom: " + sceneData.characterStats.wisdom + " Strength: " + sceneData.characterStats.strength;
+}
+/*
+	Gets the friends list from sceneData.json (for now, but will be using local storage later).
+	Outputs it in the console log area below the Twine canvas.
+
+*/
+function getFriendsList(){
+	console.log("Attempting to fetch the friends list from the data stored earlier...");
+	document.querySelector('.footer__console-status').innerHTML = consoleLogDefaultText + " " + "Friends List: " + sceneData.friendsListTab[0].firstName + ", " + sceneData.friendsListTab[1].firstName;
 }
